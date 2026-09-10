604e7fa062a9edf69ead0c028b56fed633b75b36
citmandigitaljobs <catherine.itman@gmail.com>
2026-09-11 08:55:03 +1000

    Update main.js

--- main.js
+++ main.js
@@ -228,44 +228,44 @@ span.onclick = function() {
 
 // Dad's exercise checklist
 
-const sheetName = 'Dads-exercise-checklist'
-const scriptProp = PropertiesService.getScriptProperties()
+// const sheetName = 'Dads-exercise-checklist'
+// const scriptProp = PropertiesService.getScriptProperties()
 
-function intialSetup () {
- const activeSpreadsheet = SpreadsheetApp.getActiveSpreadsheet()
- scriptProp.setProperty('key', activeSpreadsheet.getId())
-}
+// function intialSetup () {
+//  const activeSpreadsheet = SpreadsheetApp.getActiveSpreadsheet()
+//  scriptProp.setProperty('key', activeSpreadsheet.getId())
+// }
 
-function doPost (e) {
- const lock = LockService.getScriptLock()
- lock.tryLock(10000)
+// function doPost (e) {
+//  const lock = LockService.getScriptLock()
+//  lock.tryLock(10000)
 
- try {
-   const doc = SpreadsheetApp.openById(scriptProp.getProperty('key'))
-   const sheet = doc.getSheetByName(sheetName)
+//  try {
+//    const doc = SpreadsheetApp.openById(scriptProp.getProperty('key'))
+//    const sheet = doc.getSheetByName(sheetName)
 
-   const headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0]
-   const nextRow = sheet.getLastRow() + 1
+//    const headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0]
+//    const nextRow = sheet.getLastRow() + 1
 
-   const newRow = headers.map(function(header) {
-     return header === 'Date' ? new Date() : e.parameter[header]
-   })
+//    const newRow = headers.map(function(header) {
+//      return header === 'Date' ? new Date() : e.parameter[header]
+//    })
 
-   sheet.getRange(nextRow, 1, 1, newRow.length).setValues([newRow])
+//    sheet.getRange(nextRow, 1, 1, newRow.length).setValues([newRow])
 
-   return ContentService
-     .createTextOutput(JSON.stringify({ 'result': 'success', 'row': nextRow }))
-     .setMimeType(ContentService.MimeType.JSON)
- }
+//    return ContentService
+//      .createTextOutput(JSON.stringify({ 'result': 'success', 'row': nextRow }))
+//      .setMimeType(ContentService.MimeType.JSON)
+//  }
 
- catch (e) {
-   return ContentService
-     .createTextOutput(JSON.stringify({ 'result': 'error', 'error': e }))
-     .setMimeType(ContentService.MimeType.JSON)
- }
+//  catch (e) {
+//    return ContentService
+//      .createTextOutput(JSON.stringify({ 'result': 'error', 'error': e }))
+//      .setMimeType(ContentService.MimeType.JSON)
+//  }
 
- finally {
-   lock.releaseLock()
- }
-}
+//  finally {
+//    lock.releaseLock()
+//  }
+// }
 
